865837bb8941fa55dfa7a81ddc49739b5ac919c3
Schlucht <web@jagolo.ch>
2023-12-09 17:51:57 +0100

    Formular Daten erstellt

--- code/public/App/views/layouts/default.php
+++ code/public/App/views/layouts/default.php
@@ -1,14 +1,25 @@
+<?php
+
+/** @var TestCase $this */
+
+use OTS\Core\Config;
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?=CSS?>">
-    <title><?= $this->getSiteTitle()?></title>
-    <script src="<?=JS?>" type="module"></script>
+    <link rel="stylesheet" href="<?= CSS ?>">
+    <title><?= $this->getSiteTitle() ?></title>
+    <script src="<?= JS ?>" type="module"></script>
+    <?php $this->content('head'); ?>
 </head>
+
 <body>
-    <?php $this->content('header');?>
-   <?php $this->content('content');?> 
+    <?php $this->partial('partials/header');
+    ?>
+    <?php $this->content('content'); ?>
 </body>
+
 </html>
